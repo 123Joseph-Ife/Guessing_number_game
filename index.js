114bfccf284fc010caeb7a0d12b6493a13cb593e
Joseph Akinmoyero <142037969+123Joseph-Ife@users.noreply.github.com>
2023-12-24 22:05:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const toggle = document.getElementById("label");
+const body = document.querySelector(".body");
+const output = document.getElementById("output");
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+
+function changeBackground() {
+  body.classList.toggle("checked");
+  label.classList.toggle("checked");
+  output.classList.toggle("checked");
+  input.classList.toggle("checked");
+  button.classList.toggle("checked");
+}
+
+toggle.addEventListener("click", changeBackground);
+
+function guessNumber() {
+  let random = Math.floor(Math.random() * 6) + 1;
+
+  if (Number(input.value) === random) {
+    output.innerHTML = "<i>Correct, You win</i>";
+  } else {
+    output.innerHTML = `<i>You guessed ${input.value}, but the correct number is ${random}</i>`;
+  }
+}
